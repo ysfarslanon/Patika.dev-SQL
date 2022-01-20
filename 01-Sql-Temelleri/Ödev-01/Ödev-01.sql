cf0b1df855aff348617fe4fa271e29a5495d080b
kodluyoruz <arslanon@outlook.com>
2022-01-20 17:44:32 +0300

    finished ödev-1

---
+++ 01-Sql-Temelleri/Ödev-01/Ödev-01.sql
@@ -0,0 +1,21 @@
+/* patika.dev SQL modülü ödev 1 */
+
+/* Sorular
+1 - film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
+2 - film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
+3 - film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
+4 - customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
+5 - film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+*/
+
+-- Cevaplar
+-- 1. Soru
+SELECT title, description FROM film;
+-- 2. Soru
+SELECT * FROM film WHERE length > 60 AND length < 75;
+-- 3. Soru
+SELECT * FROM film WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
+-- 4. Soru
+SELECT last_name FROM customer WHERE first_name='Mary'; 
+-- 5. Soru
+SELECT * FROM film WHERE NOT length>50 AND (NOT rental_rate = 2.99 OR rental_rate = 4.99);
